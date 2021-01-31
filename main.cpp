0a9788467d9151c63ed428a9414eacb7d9b156cc
NguyenAnhDuc <nguyenanhduc.tb23@gmail.com>
2021-01-31 09:02:18 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j <= n; j++)
+            cout << setw(3) << right << setfill(' ') << j;
+        for (int j = 1; j < i; j++)
+            cout << setw(3) << right << setfill(' ') << j;
+        cout << endl;
+    }
+    return 0;
+}
